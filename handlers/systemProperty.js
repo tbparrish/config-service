e31f2f25860c5f53f7c56f0065cefd306791f5e4
Robin Duckett <robin.duckett@gmail.com>
2015-10-07 15:12:50 +0100

    Updated to serialise values

--- handlers/systemProperty.js
+++ handlers/systemProperty.js
@@ -2,7 +2,7 @@ on('SystemPropertiesGet', function (data) {
   var query = data.props instanceof Array ? { $in: data.props } : data.props;
   var properties = models.SystemProperty.findAll({ where: { name: query }});
   return properties.then(function (props) {
-    return _(props).map(function (prop) { return [prop.name, prop.value]; }).toObject();
+    return _(props).map(function (prop) { return [prop.name, JSON.parse(prop.value)]; }).toObject();
   });
 });
 
@@ -11,14 +11,14 @@ on('SystemPropertiesSet', function (data) {
   // Currently we are allowed to set any property – might want to restrict this only to existing props
   var existingProps = models.SystemProperty.findAll({ where: { name: { $in: Object.keys(data.props) }}});
   var updateExistingProps = Promise.map(existingProps, function (prop) {
-    prop.value = data.props[prop.name];
+    prop.value = JSON.stringify(data.props[prop.name]);
     return prop.save();
   });
 
   var existingKeys = Promise.map(existingProps, function (prop) { return prop.name; });
   var createNewProps = existingKeys.then(function (existing) {
     return _(data.props).filter(function (v, k) { return existing.indexOf(k) === -1; })
-                        .map(function (v, k) { return models.SystemProperty.create({ name: k, value: v }); })
+                        .map(function (v, k) { return models.SystemProperty.create({ name: k, value: JSON.stringify(v) }); })
                         .toArray();
   });
 
