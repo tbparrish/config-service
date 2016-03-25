e8a8aab85b102555caecb6d0e3659679c1cd0121
Anthony <anthony.moralez@guardtime.com>
2016-03-25 15:24:24 -0700

    remove debug logging

--- handlers/version.js
+++ handlers/version.js
@@ -9,12 +9,10 @@ on("ConfigVersion", function() {
       var version = _(stdout.split("\n")).map(function(line) {
         var parts = line.split('-');
         if (parts.length === 5) { // overwatch-auth-service-0.1.4-88.el7.centos.x86_64
-        console.log(line, parts.length, parts[3], parts[4]);
           var name = parts[1] + "-" + parts[2];
           var version = parts[3] + "-" + parts[4].split('.')[0];
           return [name, version];
         } else if (parts.length === 4) { //overwatch-services-0.1.4-73.el7.centos.x86_64
-        console.log(line, parts.length, parts[2], parts[3]);
           var ow_version = parts[2] + "-" + parts[3].split('.')[0];
           return ["Overwatch", ow_version];
         } else {
