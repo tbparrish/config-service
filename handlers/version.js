3d03d8c997a10a87e95c76f5167b8d310e9cb2a4
Anthony <anthony.moralez@guardtime.com>
2016-03-25 15:31:18 -0700

    PER-1562: use explict path to rpm and grep tools

--- handlers/version.js
+++ handlers/version.js
@@ -2,7 +2,7 @@ var exec = require("child_process").exec;
 
 on("ConfigVersion", function() {
   return Promise.promise(function(resolve, reject, notify) {
-    child = exec("rpm -qa | grep overwatch", function (error, stdout, stderr) {
+    child = exec("/usr/bin/rpm -qa | /usr/bin/grep overwatch", function (error, stdout, stderr) {
       if (error || stdout === '') {
         resolve( {'Overwatch': 'development'});
       }
