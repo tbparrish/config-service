31f13b8fbfa0d53b24895f153560490c726567c6
Anthony <anthony.moralez@guardtime.com>
2016-03-25 15:13:55 -0700

    PER-1562: presents version for each service

--- handlers/version.js
+++ handlers/version.js
@@ -2,11 +2,26 @@ var exec = require("child_process").exec;
 
 on("ConfigVersion", function() {
   return Promise.promise(function(resolve, reject, notify) {
-    child = exec('rpm -qa | grep overwatch', function (error, stdout, stderr) {
+    child = exec("rpm -qa | grep overwatch", function (error, stdout, stderr) {
       if (error || stdout === '') {
-        resolve( {'version': 'development'});
+        resolve( {'Overwatch': 'development'});
       }
-      resolve(stdout);
+      var version = _(stdout.split("\n")).map(function(line) {
+        var parts = line.split('-');
+        if (parts.length === 5) { // overwatch-auth-service-0.1.4-88.el7.centos.x86_64
+        console.log(line, parts.length, parts[3], parts[4]);
+          var name = parts[1] + "-" + parts[2];
+          var version = parts[3] + "-" + parts[4].split('.')[0];
+          return [name, version];
+        } else if (parts.length === 4) { //overwatch-services-0.1.4-73.el7.centos.x86_64
+        console.log(line, parts.length, parts[2], parts[3]);
+          var ow_version = parts[2] + "-" + parts[3].split('.')[0];
+          return ["Overwatch", ow_version];
+        } else {
+          return [];
+        }
+      }).toObject();
+      resolve(version);
     });
   });
 });
