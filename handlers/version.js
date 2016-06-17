3a51d7c59870d5d19addb1451efd077f397f438a
Anthony Moralez <anthony.moralez@guardtime.com>
2016-06-17 16:48:21 -0700

    use package.json for version

--- handlers/version.js
+++ handlers/version.js
@@ -2,22 +2,12 @@ var exec = require("child_process").exec;
 
 on("ConfigVersion", function() {
   return Promise.promise(function(resolve, reject, notify) {
-    child = exec("/usr/bin/rpm -qa | /usr/bin/grep overwatch", function (error, stdout, stderr) {
+    child = exec("for i in $(ls ../); do npm version | grep overwatch | tr -d \',:{; done", function (error, stdout, stderr) {
       if (error || stdout === '') {
         resolve( {'Overwatch': 'development'});
       }
       var version = _(stdout.split("\n")).map(function(line) {
-        var parts = line.split('-');
-        if (parts.length === 5) { // overwatch-auth-service-0.1.4-88.el7.centos.x86_64
-          var name = parts[1] + "-" + parts[2];
-          var version = parts[3] + "-" + parts[4].split('.')[0];
-          return [name, version];
-        } else if (parts.length === 4) { //overwatch-services-0.1.4-73.el7.centos.x86_64
-          var ow_version = parts[2] + "-" + parts[3].split('.')[0];
-          return ["Overwatch", ow_version];
-        } else {
-          return [];
-        }
+        return line.split(' ');
       }).toObject();
       resolve(version);
     });
