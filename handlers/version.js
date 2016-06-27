5d6e139f0d51cfa49b7cfbe25c235eacf27072ce
Anthony Moralez <anthony.moralez@guardtime.com>
2016-06-27 09:44:01 -0700

    fix version lookup

--- handlers/version.js
+++ handlers/version.js
@@ -2,8 +2,9 @@ var exec = require("child_process").exec;
 
 on("ConfigVersion", function() {
   return Promise.promise(function(resolve, reject, notify) {
-    child = exec("for i in $(ls ../); do npm version | grep overwatch | tr -d \',:{; done", function (error, stdout, stderr) {
+    child = exec("for i in $(ls ../); do cd ../$i && npm version | grep overwatch | tr -d \',:{;done", function (error, stdout, stderr) {
       if (error || stdout === '') {
+        log.debug(error);
         resolve( {'Overwatch': 'development'});
       }
       var version = _(stdout.split("\n")).map(function(line) {
