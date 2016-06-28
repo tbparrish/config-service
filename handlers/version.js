f36ae9dbe1d2a4620ee7233d9766f335b09b0688
Anthony Moralez <anthony.moralez@guardtime.com>
2016-06-27 22:38:48 -0700

    fixes version display in rpm environment

--- handlers/version.js
+++ handlers/version.js
@@ -2,13 +2,13 @@ var exec = require("child_process").exec;
 
 on("ConfigVersion", function() {
   return Promise.promise(function(resolve, reject, notify) {
-    child = exec("for i in $(ls ../); do cd ../$i && npm version | grep overwatch | tr -d \',:{;done", function (error, stdout, stderr) {
+    child = exec("for i in $(ls ../); do cd ../$i && npm version | grep overwatch | tr -d +{:,\"'\"; done", function (error, stdout, stderr) {
       if (error || stdout === '') {
-        log.debug(error);
         resolve( {'Overwatch': 'development'});
       }
+
       var version = _(stdout.split("\n")).map(function(line) {
-        return line.split(' ');
+        return line.trim().split(' ');
       }).toObject();
       resolve(version);
     });
